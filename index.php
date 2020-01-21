0144c74aae12fb5b967688320a4f428f1b505d35
fairusatoir <fairusatoir98@gmail.com>
2020-01-21 10:48:00 +0700

    antares oop v1

--- index.php
+++ index.php
@@ -2,22 +2,11 @@
 include('lib/antares-php.php');
 $antares = new Antaresphp();
 
-$antares->getAccesskey('236239f36aa1b12c:c6270c4ba2ce6849');
-$data = '{"humidity":"35","Suhu":"37"}';
-$antares->sendTo($data,'Generator','mysensor');  
-    
+$antares->set_key('236239f36aa1b12c:c6270c4ba2ce6849');
+// echo $antares->get_key();
+$antares->sendTo("hahas",'Generator','mysensor');  
+$data = $antares->viewData('Generator','mysensor');
 
-
-// if(isset($_POST['submit'])){
-//   $key = $_POST['key'];
-//   $app = $_POST['application'];
-//   $device = $_POST['device'];
-//   $sensor = $_POST['sensor'];
-//   $value = $_POST['value'];
-// }
-// else if (isset($_POST['view'])) {
-//   header("location: view.php");
-// }
 ?>
 
 <!DOCTYPE html> 
@@ -25,23 +14,11 @@ $antares->sendTo($data,'Generator','mysensor');
   <head> 
     <title>Antares GET/POST</title> 
   </head> 
-  <body>
-    <!-- Post code  -->
-    <!-- <h1>Post Data Gan</h1>
-    <form action="" method="post">
-      <div class="form-group row">
-        <br><br>
-        <input type="text" name="value" id="value" autocomplete="off" placeholder="Value" >
-      </div>
-      <br><br>
-      <div class="form-group row mt-4">
-        <button type="submit" name="submit" class="btn btn-outline-primary"><i class="fab fa-telegram-plane"></i> Post</button>
-      </div>
-    </form>
-
-    <form action="view.php">
-      <input type="submit" value="View Data" />
-  </form> -->
-    <!-- /Post code  -->    
+  <body>  
+    <h1>DATA GET dari Antares</h1>
+    <table>
+      <td>Pesan : </td>
+      <td><?= $data[0]["sensor"] ?></td>
+    </table>
   </body> 
 </html>
--- lib/antares-php.php
+++ lib/antares-php.php
@@ -1,34 +1,28 @@
 <?php 
 session_start();
 class Antaresphp {
-  public $accesskey;
+  public $header; 
 
-  function getAccesskey($accesskey){
-    $this->accesskey = $accesskey;
+  function set_key($accesskey) {
+    $this->key = $accesskey;
+  }
+  function get_key() {
+    return $this->key;
   }
 
-  // function getaccesskey(){
-  //   return $this->accesskey;
-  // }
-
   function sendTo($data,$projectName,$deviceName){
-    $dataParse = '';
-    if (gettype(json_decode($data)) == 'object'){
-      $dataParse = json_decode($data);
-      echo $data ."<br>";
-      echo "Okeyyyyy <br><br><br>";
-    }else{
-      $dataParse = $data;
-      echo "Erorrr <br><br><br>";
-    };
+    $keyacc = "{$this->key}";
 
-    $option = "{'m2m:cin': {
-           'con' : ".$data."}";
-    echo $option."<br><br>";
-    $sensor = "sensor";
-    $value = "34";
+    $header = array(
+      "X-M2M-Origin: $keyacc",
+      // "X-M2M-Origin: ",
+      "Content-Type: application/json;ty=4",
+      "Accept: application/json"
+    );
 
     $curl = curl_init();
+    $sensor = "sensor";
+    $value = "6";
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$projectName."/".$deviceName."",
       CURLOPT_RETURNTRANSFER => true,
@@ -38,85 +32,54 @@ class Antaresphp {
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS =>"{\r\n  \"m2m:cin\": {\r\n    \"con\": \"{\\\"$sensor\\\":\\\"$value\\\",\\\"$sensor\\\":\\\"$value\\\"}\"\r\n  }\r\n}",
-      CURLOPT_HTTPHEADER => array(
-        "X-M2M-Origin: 236239f36aa1b12c:c6270c4ba2ce6849",
-        "Content-Type: application/json;ty=4",
-        "Accept: application/json"
-      ),
+      CURLOPT_POSTFIELDS =>"{\r\n  \"m2m:cin\": {\r\n    \"con\": \"{\\\"$sensor\\\":\\\"$value\\\"}\"\r\n  }\r\n}",
+      CURLOPT_HTTPHEADER => $header,
     ));
-  $response = curl_exec($curl);
-  curl_close($curl);
-  echo "terkirim";
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+  }
+
+  function viewData($projectName,$deviceName){
+    $keyacc = "{$this->key}";
+    $header = array(
+      "X-M2M-Origin: $keyacc",
+      // "X-M2M-Origin: ",
+      "Content-Type: application/json;ty=4",
+      "Accept: application/json"
+    );
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$projectName."/".$deviceName."/la",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => $header,
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    // var_dump($response);
+
+    $someJSON = '['.$response.']';
+    $someArray = json_decode($someJSON, true);
+
+    // print_r($someArray);
+    // echo $someArray[0]["m2m:cin"]["con"];
+
+    $temp_url = $someArray[0]["m2m:cin"]["con"];
+    $someJSONFix = '['.$temp_url.']';
+
+    $someArrayFix = json_decode($someJSONFix, true);
+    $someArrayFix[0]["sensor"];
+    return $someArrayFix;
   }
 }
-
-// $dataTemplate  = {
-    //   'm2m:cin': {
-    //     'con' : dataParsed,
-    //   }
-    // };
-
-    // $options = {
-    //   method: 'POST',
-    //   url: `https://platform.antares.id:8443/~/antares-cse/antares-id/$projectName/$deviceName`,
-    //   headers: {
-    //     'X-M2M-Origin': $accesskey, // The access key 
-    //     'Content-Type': 'application/json;ty=4',
-    //     'Accept': 'application/json'
-    //   },
-    //   body:  json_decode($data);
-    // };
-
-    // function  callback(error, response, body) {
-    //   if(!error) {
-    //     const data = JSON.parse(body)['m2m:cin'];
-
-    //     let content = '';
-    //     try {
-    //       content = JSON.parse(data.con);
-    //     }
-    //     catch {
-    //       content = data.con;
-    //     }
-
-    //     const finalData = {
-    //       resource_identifier: data.ri,
-    //       parent_id: data.pi,
-    //       created_time: data.ct,
-    //       last_modified_time: data.lt,
-    //       content: content,
-    //     }
-        
-    //     resolve(finalData);
-    //   }
-    // }
-
-
-// function postdata($key,$app,$device,$sensor,$value){
-//   $_SESSION['key'] = $key;
-//   $_SESSION['app'] = $app;
-//   $_SESSION['device'] = $device;
-//   $curl = curl_init();
-//   curl_setopt_array($curl, array(
-//     CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$app."/".$device."",
-//     CURLOPT_RETURNTRANSFER => true,
-//     CURLOPT_ENCODING => "",
-//     CURLOPT_MAXREDIRS => 10,
-//     CURLOPT_TIMEOUT => 0,
-//     CURLOPT_FOLLOWLOCATION => true,
-//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//     CURLOPT_CUSTOMREQUEST => "POST",
-//     CURLOPT_POSTFIELDS =>"{\r\n  \"m2m:cin\": {\r\n    \"con\": \"{\\\"$sensor\\\":\\\"$value\\\"}\"\r\n  }\r\n}",
-//     CURLOPT_HTTPHEADER => array(
-//       "X-M2M-Origin:".$key,
-//       // "X-M2M-Origin: 236239f36aa1b12c:c6270c4ba2ce6849",
-//       "Content-Type: application/json;ty=4",
-//       "Accept: application/json"
-//     ),
-//   ));
-
-//   $response = curl_exec($curl);
-//   curl_close($curl);
-//   } 
 ?>
