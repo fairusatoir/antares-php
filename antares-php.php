8c11cf85b8b3048f42bdd3a65c06bea49d6d333a
fairusatoir <fairusatoir98@gmail.com>
2020-06-20 00:47:11 +0700

    Add 'GET ALL DATA' and 'GET DATA with LIMIT'

--- antares-php.php
+++ antares-php.php
@@ -1,6 +1,9 @@
 <?php 
 session_start();
 class antares_php {
+  // ==============
+  // SET KEY ACCESS
+  // ==============
   function set_key($accesskey) {
     $this->key = $accesskey;
   }
@@ -9,6 +12,9 @@ class antares_php {
     return $this->key;
   }
 
+  // ==============================
+  // SEND data to server Antares.id
+  // ==============================
   function send($data,$deviceName,$projectName){
     $keyacc = "{$this->key}";
 
@@ -21,7 +27,6 @@ class antares_php {
 
     $curl = curl_init();
     $dataSend = array(("m2m:cin") => array("con" => ($data)));
-    // $dataSend = array(("m2m:cin") => array("con" => ("$sensor:$value")));
     $data_encode = json_encode($dataSend);
     
     curl_setopt_array($curl, array(
@@ -33,27 +38,162 @@ class antares_php {
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      // CURLOPT_POSTFIELDS =>"{\r\n  \"m2m:cin\": {\r\n    \"con\": \"{\\\"$sensor\\\":\\\"$value\\\"}\"\r\n  }\r\n}",
       CURLOPT_POSTFIELDS =>$data_encode,
       CURLOPT_HTTPHEADER => $header,
     ));
+    curl_exec($curl);
+    // CHECK respone status
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if($httpCode == "404") {
+      echo "ERROR[003] : Something WRONG when SEND data";
+    }
+    curl_close($curl);
+  }
 
+  // ==========================================
+  // GET data with LIMIT from server Antares.id
+  // ==========================================
+  function get_limit($limit,$deviceName,$projectName){
+    //!!!! $limit type STRING  !!!!
+    $keyacc = "{$this->key}";
+    $header = array(
+      "X-M2M-Origin: $keyacc",
+      "Content-Type: application/json;ty=4",
+      "Accept: application/json"
+    );
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$projectName."/".$deviceName."?fu=1&ty=4&lim=".$limit, //$limit type STRING 
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => $header,
+    ));
+    //GET json Respone -> String
     $response = curl_exec($curl);
+    // CHECK respone status
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if($httpCode != "404") {
+      //CONVERT to array
+      $raw = json_decode('['.$response.']', true);
+      //REMOVE header
+      $temp_url = $raw[0]["m2m:uril"];
+      $count_temp =  count($temp_url);
+      $raw_data = [];
+      
+      //GET data
+      for($i = 0; $i < $count_temp; $i++){
+        $cin = curl_init();
+        curl_setopt_array($cin, array(
+          CURLOPT_URL => "https://platform.antares.id:8443/~".$temp_url[$i],
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => $header,
+        ));
+        //GET json Respone -> String
+        $cin_res = curl_exec($cin);
+        //CONVERT to array
+        $raw = json_decode('['.$cin_res.']', true);
+        $raw_json = json_decode($raw[0]["m2m:cin"]["con"],true);
+        //ADD data to array
+        array_push($raw_data,$raw_json);
+        curl_close($cin);  
+      }
+      return $raw_data; //-> Array
+    }else{
+      echo "ERROR[004] : Application Name or Device Name is Wrong";
+    }
     curl_close($curl);
   }
 
 
-  function get($deviceName,$projectName){
+  // ===================================
+  // GET ALL data from server Antares.id
+  // ===================================
+  function get_all($deviceName,$projectName){
     $keyacc = "{$this->key}";
     $header = array(
       "X-M2M-Origin: $keyacc",
-      // "X-M2M-Origin: ",
       "Content-Type: application/json;ty=4",
       "Accept: application/json"
     );
 
     $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$projectName."/".$deviceName."?fu=1&ty=4",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => $header,
+    ));
+    //GET json Respone -> String
+    $response = curl_exec($curl);
+    // CHECK respone status
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if($httpCode != "404") {
+      //CONVERT to array
+      $raw = json_decode('['.$response.']', true);
+      //REMOVE header
+      $temp_url = $raw[0]["m2m:uril"];
+      $count_temp =  count($temp_url);
+      $raw_data = [];
+      
+      //GET data
+      for($i = 0; $i < $count_temp; $i++){
+        $cin = curl_init();
+        curl_setopt_array($cin, array(
+          CURLOPT_URL => "https://platform.antares.id:8443/~".$temp_url[$i],
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => $header,
+        ));
+        //GET json Respone -> String
+        $cin_res = curl_exec($cin);
+        //CONVERT to array
+        $raw = json_decode('['.$cin_res.']', true);
+        $raw_json = json_decode($raw[0]["m2m:cin"]["con"],true);
+        //ADD data to array
+        array_push($raw_data,$raw_json);
+        curl_close($cin);  
+      }
+      return $raw_data; //-> Array
+    }else{
+      echo "ERROR[001] : Application Name or Device Name is Wrong";
+    }
+    curl_close($curl);
+  }
 
+  // ====================================
+  // GET LAST data from server Antares.id
+  // ====================================
+
+  function get($deviceName,$projectName){
+    $keyacc = "{$this->key}";
+    $header = array(
+      "X-M2M-Origin: $keyacc",
+      "Content-Type: application/json;ty=4",
+      "Accept: application/json"
+    );
+
+    $curl = curl_init();
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://platform.antares.id:8443/~/antares-cse/antares-id/".$projectName."/".$deviceName."/la",
       CURLOPT_RETURNTRANSFER => true,
@@ -65,23 +205,24 @@ class antares_php {
       CURLOPT_CUSTOMREQUEST => "GET",
       CURLOPT_HTTPHEADER => $header,
     ));
-
+    //GET json String
     $response = curl_exec($curl);
+    // CHECK respone status
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if($httpCode != "404") {
+      //CONVERT to array
+      $raw = json_decode('['.$response.']', true);
 
-    curl_close($curl);
-    // var_dump($response);
+      //REMOVE header
+      $temp_url = $raw[0]["m2m:cin"]["con"];
+      $JSON = json_decode('['.$temp_url.']',true);
+      curl_close($curl);
+      return $JSON; //-> Array
+    }else{
+      echo "ERROR[002] : Application Name or Device Name is Wrong";
+    }
 
-    $someJSON = '['.$response.']';
-    $someArray = json_decode($someJSON, true);
-
-    // print_r($someArray);
-    // echo $someArray[0]["m2m:cin"]["con"];
-
-    $temp_url = $someArray[0]["m2m:cin"]["con"];
-    $someJSONFix = '['.$temp_url.']';
-
-    $someArrayFix = json_decode($someJSONFix, true);
-    return $someArrayFix;
+    
   }
 }
 ?>
--- index.php
+++ index.php
@@ -1,32 +1,124 @@
 <?php
+
 include('antares-php.php');
 $antares = new antares_php();
-
 $antares->set_key('your-access-key-here');
 
-$yourdata = '{"Sensor1":"Value1","Sensor2":"Value2"}';
-
-$antares->send($yourdata,'your-device-name', 'your-application-name');  
-
+// $yourdata = '{"Sensor1":"Value1","Sensor2":"Value2"}';
+// $antares->send($yourdata,'DevicePHP', 'AntaresPHP');  
 
 $yourdata = $antares->get('your-device-name', 'your-application-name');
-$yourdata_encode = json_encode($yourdata); //Json to String
+$yourall = $antares->get_all('your-device-name', 'your-application-name');
 
+if(array_key_exists('limit', $_POST)) { 
+  $limit = $_POST["limit"];
+  $yourlimit = $antares->get_limit($limit,'your-device-name', 'your-application-name');
+}
 ?>
+
 <!DOCTYPE html> 
 <html lang="en"> 
   <head> 
     <title>Antares GET/POST</title> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <style>
+    .container {
+      padding-top:100px;
+    }
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      padding: 8px;
+      text-align: center;
+      border-bottom: 1px solid #ddd;
+    }
+    h1 {
+      text-align: center;
+    }
+    input { 
+      text-align: center; 
+    }
+    </style>
   </head> 
-  <body>  
-    <h1>GET Data Antares</h1>
-    <table>
-      <td>Message : </td>
-      <td>
-      <?php
-        echo $Viewdata_encode
-      ?>
-      </td>
-    </table>
+  <body>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h1>GET ALL Data Antares</h1>
+          <table>
+            <tr>
+              <th>Label</th>
+              <th>Value</th>
+            </tr>
+            <?php
+              foreach ($yourall as $key => $value) {
+            ?>
+            <tr>
+                <td>
+                  <?php
+                    echo $value["Sensor1"]  
+                  ?>
+                </td>
+                <td>
+                  <?php
+                    echo $value["Sensor2"]
+                  ?>
+                </td>
+            </tr>
+            <?php
+              }
+            ?>
+            </td>
+          </table>
+        </div>
+
+        <div class="col-md-6">
+          <h1>GET Data Antares with limit</h1>
+          <table>
+            <form method="post">
+              <tr>
+                <th>LIMIT : </th>
+                <th>
+                  <input type="text" id="limit" name="limit" value=0><br>
+                </th>
+                <th> 
+                  <input type="submit" name="button1" class="button" value="GET Limit" />  
+                </th>
+              </tr>
+            </form> 
+          </table>
+          <br><br>
+          <h1>RESULT</h1>
+          <table>
+            <tr>  
+              <th>Label</th>
+              <th>Value</th>
+            </tr>
+            <?php
+              foreach ($yourlimit as $key => $value) {
+            ?>
+            <tr>
+              <td>
+                <?php
+                  echo $value["Sensor1"]  
+                ?>
+              </td>
+              <td>
+                <?php
+                  echo $value["Sensor2"]
+                ?>
+              </td>
+            </tr>
+            <?php
+              }
+            ?>
+          </table>
+        </div>
+
+      </div>
+    </div>  
   </body> 
 </html>
